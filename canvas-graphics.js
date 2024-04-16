a15ca661cf04b34c8b76f4fed32ea06ddf52e043
jamesweber7 <jaweber7@asu.edu>
2024-04-15 21:00:09 -0700

    functionality for drawing shapes

--- canvas-graphics.js
+++ canvas-graphics.js
@@ -1,18 +1,97 @@
 const tr = {    // transform
     x: 0,
     y: 0,
-    sc: 0
+    sc: 1
 };
+let unit;
+
+var shapes = [
+    /*
+    point: {
+        type,
+        x,
+        y
+    },
+    line: {
+        type,
+        p1: point,
+        p2: p
+    },
+    arc: {
+        type,
+        x,
+        y,
+        r,
+        start_theta,
+        end_theta
+    }
+    */
+];
+
+const SHAPE_TYPES = {
+    POINT: 'POINT',
+    LINE: 'LINE',
+    ARC: 'ARC',
+}
 
 function setup() {
     createCanvas(windowWidth*0.999, windowHeight*0.999);
-}
-
-function windowResized() {
-    createCanvas(windowWidth*0.999, windowHeight*0.999);
+    setSizing();
 }
 
 function draw() {
     background(255);
-    circle(mouseX, mouseY, 10);
+
+    drawShapes();
+
+    drawCursor();
+}
+
+function drawShapes() {
+    push();
+    translate(tr.x, tr.y);
+    scale(tr.sc);
+
+    shapes.forEach(shape => {
+        drawShape(shape);
+    })
+
+    pop();
+}
+
+function drawShape(shape) {
+    switch (shape.type) {
+        case SHAPE_TYPES.POINT:
+            return drawPoint(shape);
+        case SHAPE_TYPES.LINE:
+            return drawLine(shape);
+        case SHAPE_TYPES.ARC:
+            return drawArc(shape);
+    }
+}
+
+function drawPoint(pt) {
+    const r = unit*2;
+    circle(pt.x, pt.y, r);
+}
+
+function drawLine(line_) {
+    line(line_.p1.x, line_.p1.y, line_.p2.x, line_.p2.y);
+}
+
+function drawArc(arc_) {
+    arc(arc_.x, arc_.y, arc_.r, arc_.r, arc_.start_theta, arc_.end_theta);
+}
+
+function drawCursor() {
+    text(getMouseMode()[0], mouseX+5, mouseY)
+}
+
+function windowResized() {
+    createCanvas(windowWidth*0.999, windowHeight*0.999);
+    setSizing();
+}
+
+function setSizing() {
+    unit = width / 1500;
 }
--- canvas-interaction.js
+++ canvas-interaction.js
@@ -15,3 +15,149 @@ function setMouseMode(mode) {
 function getMouseMode() {
     return mouseMode;
 }
+
+function mousePressed() {
+    switch (getMouseMode()) {
+        case MOUSE_MODES.SELECT:
+            return selectMousePressed();
+        case MOUSE_MODES.POINT:
+            return pointMousePressed();
+        case MOUSE_MODES.RULER:
+            return rulerMousePressed();
+        case MOUSE_MODES.COMPASS:
+            return compassMousePressed();
+        case MOUSE_MODES.ERASER:
+            return eraserMousePressed();
+    }
+}
+
+function mouseReleased() {
+    switch (getMouseMode()) {
+        case MOUSE_MODES.SELECT:
+            return selectMouseReleased();
+        case MOUSE_MODES.POINT:
+            return pointMouseReleased();
+        case MOUSE_MODES.RULER:
+            return rulerMouseReleased();
+        case MOUSE_MODES.COMPASS:
+            return compassMouseReleased();
+        case MOUSE_MODES.ERASER:
+            return eraserMouseReleased();
+    }
+}
+
+function mouseDragged() {
+    switch (getMouseMode()) {
+        case MOUSE_MODES.SELECT:
+            return selectMouseDragged();
+        case MOUSE_MODES.POINT:
+            return pointMouseDragged();
+        case MOUSE_MODES.RULER:
+            return rulerMouseDragged();
+        case MOUSE_MODES.COMPASS:
+            return compassMouseDragged();
+        case MOUSE_MODES.ERASER:
+            return eraserMouseDragged();
+    }
+}
+
+function mouseMoved() {
+    switch (getMouseMode()) {
+        case MOUSE_MODES.SELECT:
+            return selectMouseMoved();
+        case MOUSE_MODES.POINT:
+            return pointMouseMoved();
+        case MOUSE_MODES.RULER:
+            return rulerMouseMoved();
+        case MOUSE_MODES.COMPASS:
+            return compassMouseMoved();
+        case MOUSE_MODES.ERASER:
+            return eraserMouseMoved();
+    }
+}
+
+
+// Select tool mouse events
+function selectMousePressed() {
+
+}
+
+function selectMouseReleased() {
+
+}
+
+function selectMouseDragged() {
+
+}
+
+function selectMouseMoved() {
+
+}
+
+// Point tool mouse events
+function pointMousePressed() {
+
+}
+
+function pointMouseReleased() {
+
+}
+
+function pointMouseDragged() {
+
+}
+
+function pointMouseMoved() {
+
+}
+
+// Ruler tool mouse events
+function rulerMousePressed() {
+
+}
+
+function rulerMouseReleased() {
+
+}
+
+function rulerMouseDragged() {
+
+}
+
+function rulerMouseMoved() {
+
+}
+
+// Compass tool mouse events
+function compassMousePressed() {
+
+}
+
+function compassMouseReleased() {
+
+}
+
+function compassMouseDragged() {
+
+}
+
+function compassMouseMoved() {
+
+}
+
+// Eraser tool mouse events
+function eraserMousePressed() {
+
+}
+
+function eraserMouseReleased() {
+
+}
+
+function eraserMouseDragged() {
+
+}
+
+function eraserMouseMoved() {
+
+}
